25264043cdf78a995187eb853cbc261c5c7d7bfc
loilo <florian.reuschel@gmx.de>
2024-05-08 10:07:54 +0200

    feat: update dependencies, add support for PHP 8.3, drop support for PHP 5.6

--- util/formatting-worker/formatting-utils.ts
+++ util/formatting-worker/formatting-utils.ts
@@ -44,7 +44,7 @@ export function validateMessage(
   }
 
   if (
-    !['5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'].includes(
+    !['7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3'].includes(
       message.version as string,
     )
   ) {
