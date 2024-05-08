9dce61cd07277756e64d44cb962498294a0351a1
loilo <florian.reuschel@gmx.de>
2024-05-08 10:14:28 +0200

    fix: correctly report errors in latest @php-wasm/web version

--- util/evaluation-worker/evaluation-worker.ts
+++ util/evaluation-worker/evaluation-worker.ts
@@ -14,7 +14,21 @@ addEventListener('message', async event => {
     getCachedPhpEngine(data.baseUrl, data.version),
     getCachedComposerSetupFunction(data.version),
   ] as const)
-  const result = await evaluate(engine, data.code, setupFunction)
+
+  let result: string
+  try {
+    result = await evaluate(engine, data.code, setupFunction)
+  } catch (error) {
+    const message = String((error as any).message ?? error)
+    const errorIdentifier = 'the following output: '
+    if (message.includes(errorIdentifier)) {
+      result = message
+        .slice(message.indexOf(errorIdentifier) + errorIdentifier.length)
+        .replaceAll('/arena/index.php', 'PHP Arena code')
+    } else {
+      result = message
+    }
+  }
 
   postMessage({
     type: 'evaluated',
